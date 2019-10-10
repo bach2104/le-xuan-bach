bae2b0ae6d8e2ac3f092aa351f587a7353a911ed
bach2104 <lexuanbach2104@gmail.com>
2019-10-10 15:18:17 +0700

    bai tap on tap

---
+++ so am so duong.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+	int main(){
+		int a;
+		printf("Nhap vao mot so:\n");
+		scanf("%d", &a);
+		if (a>0) printf("Day la so duong:%d", a);
+		else if (a==0) printf("Khong xac dinh:%d", a);
+		else if (a<0) printf("Day la so am:%d", a);
+		
+		
+		
+		
+		
+		
+		return 0;
+		
+		}
